085f123d8fbce0d43ac9a94ba4015a742a257e1c
nataliizab <nataliia.zab@gmail.com>
2023-05-10 22:11:12 +0100

    added style to the dark mode button

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,8 +11,19 @@ const Header = ({ darkMode, setDarkMode }) => {
             className="dark-mode-button"
             onClick={() => setDarkMode(!darkMode)}
           >
-            {darkMode ? "Light Mode" : "Dark Mode"}
-            
+            {darkMode ? (
+              <img
+                src="https://www.vhv.rs/dpng/d/0-3029_sun-icon-transparent-background-hd-png-download.png"
+                alt="Light Mode"
+                className="dark-mode-icon"
+              />
+            ) : (
+              <img
+                src="https://cdn.icon-icons.com/icons2/1674/PNG/512/moon_111148.png"
+                alt="Dark Mode"
+                className="dark-mode-icon"
+              />
+            )}
           </button>
         </div>
       </div>
